e3e5d6cae8a44a27c437d2373b53831d73a9a687
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-29 15:01:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@ suite_e: "Crumpet the Elf"
 def select_winner(passengers)
   winner = ""
   passangers.each do |suite, name|
-    if siute == :suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       winner = name
     end
 end
